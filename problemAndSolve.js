f3adffcd472d5fd7d7e06d4f5eeb9eb3699fbc82
2md-mahmudul-hasan <2md.mahmudul.hasan@gmail.com>
2023-01-27 17:34:48 +0600

    initial commit

---
+++ problemAndSolve.js
@@ -0,0 +1,68 @@
+//course link https://drive.google.com/drive/folders/1MW45PoaR9-IAQeREgXz4tu3BkUbnytf1
+1; // Write code to get arrary on names from given array of users
+
+const users = [
+  {
+    id: 1,
+    name: "balul",
+    isActive: true,
+  },
+  {
+    id: 2,
+    name: "kabul",
+    isActive: true,
+  },
+  {
+    id: 3,
+    name: "mabul",
+    isActive: true,
+  },
+  {
+    id: 3,
+    name: "jabul",
+    isActive: true,
+  },
+];
+
+let namesArray = [];
+
+// for (let i = 0; i < users.length; i++) {
+//   namesArray.push(users[i].name);
+// }
+// console.log(namesArray);
+
+// users.forEach((user) => {
+//   namesArray.push(user.name);
+// });
+
+// console.log(namesArray);
+
+// const name = users.map((user) => user.name);
+// console.log(name);
+
+// 2. let name ;
+// console.log(name)
+// console.log(typeof(name))
+
+// let name = null
+// console.log(name)
+// console.log(typeof(name))
+
+// what is the result?//difference between null and undifined?
+
+// 3. What is console.log here?
+
+// console.log(foo)
+//  foo = 1;
+// ans: ReferenceError
+// 4. console.log(foo)
+// var foo = 3;
+// ans: undefined;
+
+// 5.
+// console.log(foo)
+// var foo = 5;
+// is like:
+// foo = 5;
+// console.log(foo)
+// var foo;
